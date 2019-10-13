0e119fddff619cf7815b2ddd325f42a1385f0d0b
Alisson Bittencourt <alison_btt@hotmail.com>
2019-10-13 17:37:57 -0300

    'final_aula_405'

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import Speaker
+from django.utils.html import format_html
+from .models import Speaker, Contact
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
+
 
 class SpeakerModelAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]
     prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'photo_img', 'website_link']
+
+    def website_link(self, obj):
+        return format_html('<a href="{0}">{0}</a>', obj.website)
+
+    website_link.short_description = 'website'
+
+    def photo_img(self, obj):
+        return format_html ('<img width="32px" src="{}" />', obj.photo)
+
+    photo_img.short_description = 'foto'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
 
