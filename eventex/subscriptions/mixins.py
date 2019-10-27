57441c72bd025d9c33fef39ef5dff7d2f0f5b9d9
Alisson Bittencourt <alison_btt@hotmail.com>
2019-10-27 13:33:11 -0300

    'correcao_final_aula_411'

--- eventex/subscriptions/mixins.py
+++ eventex/subscriptions/mixins.py
@@ -5,36 +5,36 @@ from django.views.generic.edit import CreateView
 
 
 class EmailCreateMixin:
-    email_to = None
-    email_context_name = None
-    email_tamplate_name = None
-    email_from = settings.DEFAULT_FROM_EMAIL
     email_subject = ''
+    email_tamplate_name = None
+    email_context_name = None
+    email_from = settings.DEFAULT_FROM_EMAIL
+    email_to = None
+
 
     def send_mail(self):
         # Send Email
+        template_name = self.get_email_tamplate_name()
         subject = self.email_subject
         from_ = self.email_from
         to = self.get_email_to()
-        template_name = self.get_email_tamplate_name()
         context = self.get_email_context_data()
-
-        bady = render_to_string(template_name, context)
-        return mail.send_mail(subject, bady, from_, [from_, to])
+        body = render_to_string(template_name, context)
+        mail.send_mail(subject, body, from_, [from_, to])
 
     def get_email_tamplate_name(self):
         if self.email_tamplate_name:
             return self.email_tamplate_name
 
         meta = self.object._meta
-        return '{}/{}_email.txt'.format(meta.app_label, meta.model_name)
 
+        return f'{meta.app_label}/{meta.model_name}_email.txt'
 
     def get_email_context_data(self, *args, **kwargs):
         context = dict(kwargs)
         context.setdefault(self.get_email_context_name(), self.object)
         return context
-
+w
     def get_email_context_name(self):
         if self.email_context_name:
             return self.email_context_name
@@ -44,6 +44,7 @@ class EmailCreateMixin:
         if self.email_to:
             return self.email_to
         return self.object.email
+
 
 class EmailCreateView(EmailCreateMixin, CreateView):
     def form_valid(self, form):
